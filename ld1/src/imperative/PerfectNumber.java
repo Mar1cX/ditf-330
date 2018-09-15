e98619c9c458901e3d73dfd73ddb6b9ea4773928
Mar1cX <ruckomaris@yahoo.com>
2018-09-15 19:23:16 +0300

    Added PerfectNumber project without fuctional programming

---
+++ ld1/src/imperative/PerfectNumber.java
@@ -0,0 +1,33 @@
+package imperative;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class PerfectNumber {
+    public enum STATE { ABUNDANT, DEFICIENT, PERFECT; }
+
+    public static Set<Integer> divisors(int n) {
+        Set divisors_set = new HashSet();
+        for(int i = 1; i <= n; i++) {
+            if (n % i == 0) { divisors_set.add(i); }
+        }
+        return divisors_set;
+    }
+
+    public static STATE process(int n) {
+        int summerized_divisors = divisors(n).stream().mapToInt(Integer::intValue).sum() / 2;
+
+        if (summerized_divisors == n) {
+            return STATE.PERFECT;
+        } else if (summerized_divisors < n) {
+            return STATE.DEFICIENT;
+        } else {
+            return STATE.ABUNDANT;
+        }
+    }
+
+    public static STATE detect(int n) {
+        divisors(n);
+        return process(n);
+    }
+}
