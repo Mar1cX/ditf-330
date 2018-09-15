1fbf2755de85c1bafad63a1de61ebe110587944c
Mar1cX <ruckomaris@yahoo.com>
2018-09-15 21:55:35 +0300

    First attempt making it as functional programming

--- ld1/src/imperative/PerfectNumber.java
+++ ld1/src/imperative/PerfectNumber.java
@@ -1,34 +1,49 @@
 package imperative;
 
 import java.util.Set;
-import java.util.HashSet;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class PerfectNumber {
-    public enum STATE { ABUNDANT, DEFICIENT, PERFECT, ERROR; }
+interface PerfectNumberInterface {
+
+    default String validPerfect(int i, int n) {
+        return "TEST";
+    }
+
+    default String validDeficient(int i, int n) {
+        return "TEST";
+    }
+}
+
+public class PerfectNumber implements PerfectNumberInterface {
+    public enum STATE { PERFECT, DEFICIENT, ABUNDANT, ERROR; }
+
+    public static STATE isPerfect(int sum, int n) {
+        Predicate<Integer> isEqual = i -> i == n;
+        return isEqual.test(sum) ? STATE.PERFECT : isDeficient(sum, n);
+    }
+
+    public static STATE isDeficient(int sum, int n) {
+        Predicate<Integer> isLess = i -> i < n;
+        return isLess.test(sum) ? STATE.DEFICIENT : STATE.ABUNDANT;
+    }
 
     public static Set<Integer> divisors(int n) {
-        Set divisors_set = new HashSet();
-        for(int i = 1; i <= n; i++) {
-            if (n % i == 0) { divisors_set.add(i); }
-        }
-        return divisors_set;
+        return IntStream
+                .range(1, n + 1).filter(i -> n % i == 0)
+                .boxed().collect(Collectors.toSet());
     }
 
     public static STATE process(int n) {
-        int summerized_divisors = divisors(n).stream().mapToInt(Integer::intValue).sum() / 2;
-
-        if (summerized_divisors == n) {
-            return STATE.PERFECT;
-        } else if (summerized_divisors < n) {
-            return STATE.DEFICIENT;
-        } else {
-            return STATE.ABUNDANT;
-        }
+        return isPerfect(divisors(n).stream().mapToInt(Integer::intValue).sum() / 2, n);
     }
 
     public static STATE detect(int n) {
+        Predicate<Integer> isNegative = i -> i <= 0;
+
         try {
-            if (n <= 0) { throw new java.lang.Error("Skaitlis nav pozitivs"); };
+            if (isNegative.test(n)) { throw new java.lang.Error("Skaitlis nav pozitivs"); };
             return process(n);
         } catch(Error e) {
             return STATE.ERROR;
