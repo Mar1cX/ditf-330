b327bd11f030a141859f9e2e20f3c16d4c7ad60f
Mar1cX <ruckomaris@yahoo.com>
2018-09-16 11:22:48 +0300

    Replaced STATE if/else if/else blocks with different approach

--- ld1/src/imperative/PerfectNumber.java
+++ ld1/src/imperative/PerfectNumber.java
@@ -1,32 +1,21 @@
 package imperative;
 
-import java.util.Set;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-interface PerfectNumberInterface {
+public class PerfectNumber {
+    public enum STATE {
+        PERFECT(0), DEFICIENT(-1), ABUNDANT(1), ERROR(2);
+        private final int value;
+        STATE(int value) { this.value = value; }
 
-    default String validPerfect(int i, int n) {
-        return "TEST";
-    }
-
-    default String validDeficient(int i, int n) {
-        return "TEST";
-    }
-}
-
-public class PerfectNumber implements PerfectNumberInterface {
-    public enum STATE { PERFECT, DEFICIENT, ABUNDANT, ERROR; }
-
-    public static STATE isPerfect(int sum, int n) {
-        Predicate<Integer> isEqual = i -> i == n;
-        return isEqual.test(sum) ? STATE.PERFECT : isDeficient(sum, n);
-    }
-
-    public static STATE isDeficient(int sum, int n) {
-        Predicate<Integer> isLess = i -> i < n;
-        return isLess.test(sum) ? STATE.DEFICIENT : STATE.ABUNDANT;
+        public static Optional<STATE> valueOf(int value) {
+            return Arrays.stream(values())
+                    .filter(STATE -> STATE.value == value)
+                    .findFirst();
+        }
     }
 
     public static Set<Integer> divisors(int n) {
@@ -36,14 +25,15 @@ public class PerfectNumber implements PerfectNumberInterface {
     }
 
     public static STATE process(int n) {
-        return isPerfect(divisors(n).stream().mapToInt(Integer::intValue).sum() / 2, n);
+        Function<Integer, Integer> toSignum = i -> Integer.signum(i);
+        int sum = divisors(n).stream().mapToInt(Integer::intValue).sum() / 2;
+        return STATE.valueOf(toSignum.apply(sum - n)).get();
     }
 
     public static STATE detect(int n) {
-        Predicate<Integer> isNegative = i -> i <= 0;
-
+        Predicate<Integer> isNegativeOrZero = i -> i <= 0;
         try {
-            if (isNegative.test(n)) { throw new java.lang.Error("Skaitlis nav pozitivs"); };
+            if (isNegativeOrZero.test(n)) throw new java.lang.Error("Negative number");
             return process(n);
         } catch(Error e) {
             return STATE.ERROR;
