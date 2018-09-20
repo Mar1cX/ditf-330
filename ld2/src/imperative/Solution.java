0ceeafa9b18ddf06ffbd16ed4e6debe8d3ecb422
Mar1cX <ruckomaris@yahoo.com>
2018-09-20 14:39:18 +0300

    Added few Java Tests

--- ld2/src/imperative/Solution.java
+++ ld2/src/imperative/Solution.java
@@ -5,24 +5,26 @@ import java.util.Optional;
 import java.util.stream.IntStream;
 
 public class Solution {
-    static int res = 0;
+    public static int res;
 
-    static int filterRecursive(int current_number, int N, int X, int position) {
-        int[] idx = { position + 1 };
+    public static int filterRecursive(int current_number, int N, int X, int position) {
         Optional.of(X).filter(i -> i == 0).map(i -> res++);
+        int[] idx = { position + 1 };
+        int r = (int) Math.floor(Math.pow(current_number, 1.0 / N));
 
-        IntStream.rangeClosed(position + 1, (int) Math.floor(Math.pow(current_number, 1.0 / N)))
+        IntStream.rangeClosed(position + 1, r)
                  .map(i -> X - (int) Math.pow(i, N))
                  .filter(a -> a >= 0)
                  .forEach(a -> {
-                     filterRecursive(current_number, X - (int) Math.pow(idx[0], N), idx[0], N);
+                     filterRecursive(current_number, N, X - (int) Math.pow(idx[0], N), idx[0]);
                     idx[0]++;
                 });
 
         return res;
     }
 
-    static int powerSum(int X, int N) {
+    public static int powerSum(int X, int N) {
+        res = 0;
         return filterRecursive(X, N, X, 0);
     }
 
